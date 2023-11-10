b20d7f46c78b6469f0751e39e9e882168a2e16c8
Sneekit <mossjde@gmail.com>
2023-11-09 17:13:16 -0700

    Expanded run.sh to only run program once

--- run.sh
+++ run.sh
@@ -1,1 +1,8 @@
-nohup python3 gpiotest.py &
+# this will run the program in the background
+
+if [ $(pgrep -f "gpiotest.py") ]; then
+	echo "gpiotest.py already running."
+else
+	nohup python3 gpiotest.py &
+	echo "gpiotest.py started"
+fi
